915bbbd99a4b2b4efe49589e26c6eab07e8a1128
SabariMathiyalagan <sabari.mathi30@gmail.com>
2025-10-04 21:48:52 -0400

    Simulation code

--- interrupts.cpp
+++ interrupts.cpp
@@ -2,10 +2,14 @@
  *
  * @file interrupts.cpp
  * @author Sasisekhar Govind
+ * @author Sabari Mathiyalagan
+ * @author George Tzemenakis
+
  *
  */
 
 #include<interrupts.hpp>
+#include <algorithm> 
 
 int main(int argc, char** argv) {
 
@@ -19,6 +23,9 @@ int main(int argc, char** argv) {
     std::string execution;  //!< string to accumulate the execution output
 
     /******************ADD YOUR VARIABLES HERE*************************/
+    int current_time = 0;
+    int context_save_time = 10;
+    int isr_runtime = 40;
 
 
 
@@ -29,16 +36,52 @@ int main(int argc, char** argv) {
         auto [activity, duration_intr] = parse_trace(trace);
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
+        if (activity == "CPU") {
+            execution += std::to_string(current_time) + ", " + std::to_string(duration_intr) + ", CPU Burst\n";
+            current_time += duration_intr;
+        } else if (activity == "SYSCALL" || activity == "END_IO") {
+            int device_number = duration_intr;
+            int total_time_left = delays[device_number];
+
+            auto [boilerplate_text, new_time] = intr_boilerplate(current_time, device_number, context_save_time, vectors);
+            execution += boilerplate_text;
+            current_time = new_time;
 
 
+            if (activity == "SYSCALL") {
+            int run_isr_time = std::min(total_time_left, isr_runtime);
+            execution += std::to_string(current_time) + ", " + std::to_string(run_isr_time) + ", SYSCALL: run the ISR (device driver)\n";
+            current_time += run_isr_time;
+            total_time_left -= run_isr_time;
 
+            int transfer_data_time = std::min(total_time_left, isr_runtime);
+            execution += std::to_string(current_time) + ", " + std::to_string(transfer_data_time) + ", transfer data from device to memory\n";
+            current_time += transfer_data_time;
+            total_time_left -= transfer_data_time;
+
+            execution += std::to_string(current_time) + ", " + std::to_string(total_time_left) + ", check for errors\n";
+            current_time += total_time_left;
+            }
+            else {
+                int run_isr_time = std::min(total_time_left, isr_runtime);
+                execution += std::to_string(current_time) + ", " + std::to_string(run_isr_time) + ", ENDIO: run the ISR (device driver)\n";
+                current_time += run_isr_time;
+                total_time_left -= run_isr_time;
+                execution += std::to_string(current_time) + ", " + std::to_string(total_time_left) + ", check device status\n";
+                current_time += total_time_left;
+
+            }
+
+            
         /************************************************************************/
 
     }
 
-    input_file.close();
+    
+}
+        input_file.close();
 
-    write_output(execution);
+        write_output(execution);
 
-    return 0;
+        return 0;
 }
